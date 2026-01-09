deda463edbb674a4ff38b4448b157ee4c772e086
Janu Nagarajan <janu.nagarajan@aspiresys.com>
2026-01-09 22:53:51 +0530

    09-01-2026 PHP first project creation

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Initial PHP setup";
-?>
+<!doctype>
+<html>
+
+<body>
+    <p>This is html</p>
+    <?php echo 'php' ?>
+</body>
+
+</html>
+
+<!-- <?php echo 'php' ?> -->
+
+<!-- If we place php code  will be procossed between html and outside the html code also -->fg
