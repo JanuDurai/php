21f463ba73690aabc2bbc8a736b8084c26ba11d4
Janu Nagarajan <janu.nagarajan@aspiresys.com>
2026-01-27 11:49:02 +0530

    php oops

--- oops.php
+++ oops.php
@@ -8,11 +8,15 @@ class firstClass
 
     public function displayVar()
     {
-        echo $this->var,"\n";
+        echo $this->var, "\n";
     }
+
+    public string $name { set => trim($value); }
+
 }
 // object creation
 $object = new firstClass();
+$object->var = "variable value changed through object";
 $object->displayVar();
 
 class SimpleClass
@@ -36,5 +40,53 @@ $extendedClassObject = new ExtendClass();
 $extendedClassObject->displayVar();
 
 
+// Constructors and destructors
+class User
+{
+    function __construct($name)
+    {
+        $this->name = $name;
+        echo $this->name, "\n";
+    }
 
-?>
+    function __destruct()
+    {
+        echo 'Class is destroyed', "\n";
+    }
+}
+
+$userObject = new User("JAnu");
+
+// static keyword
+
+class Counter
+{
+    static $count = 0;
+    static function inc()
+    {
+        self::$count++;
+        echo "counter is ", self::$count, "\n";
+
+    }
+
+}
+
+$counter = new Counter();
+
+$counter->inc();
+
+// ananymous class
+
+$ananymous_class_obj = new class {
+    public function hi(): void
+    {
+        echo "Hello Ananymous Class", "\n";
+    }
+}
+
+// object clone
+
+$counter_clone = clone $counter;
+
+
+    ?>
